e7909ce0faa6da5730b5eb5cb9c75ba20fc362d5
Josue Abraham <josuea@noukod.com>
2018-08-28 20:33:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   row = "   |   |   "
-  separator = "----------"
+  separator = "-----------"
   puts row
   puts separator
   puts row
